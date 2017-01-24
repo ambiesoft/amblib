4cc95bbaa228d9af341b144a1a46d3b1dd6c9df2
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2017-01-24 13:57:19 +0000

    iiii
    git-svn-id: svn://192.168.0.99/amblib@6697 5ada3c48-002d-0410-850d-99865cc6fd0f

--- amblib/AmbLib.cs
+++ amblib/AmbLib.cs
@@ -7,6 +7,7 @@ namespace Ambiesoft
     using System.Drawing;
     using System.Windows.Forms;
     using System.IO;
+    using Microsoft.Win32;
 
     public class AmbLib
     {
@@ -111,5 +112,76 @@ namespace Ambiesoft
             string file = Path.GetFileNameWithoutExtension(fi.Name) + ".ini";
             return Path.Combine(dir, file);
         }
-    }
-}
+
+
+        public static string GetInstalledDotNetVersionFromRegistry()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // Opens the registry key for the .NET Framework entry.
+            using (RegistryKey ndpKey =
+                RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").
+                OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                // As an alternative, if you know the computers you will query are running .NET Framework 4.5 
+                // or later, you can use:
+                // using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, 
+                // RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        string name = (string)versionKey.GetValue("Version", "");
+                        string sp = versionKey.GetValue("SP", "").ToString();
+                        string install = versionKey.GetValue("Install", "").ToString();
+                        if (install == "") //no install info, must be later.
+                            sb.AppendLine(versionKeyName + "  " + name);
+                        else
+                        {
+                            if (sp != "" && install == "1")
+                            {
+                                
+                                sb.AppendLine(versionKeyName + "  " + name + "  SP" + sp);
+                            }
+
+                        }
+                        if (name != "")
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (name != "")
+                                sp = subKey.GetValue("SP", "").ToString();
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (install == "") //no install info, must be later.
+                                sb.AppendLine(versionKeyName + "  " + name);
+                            else
+                            {
+                                if (sp != "" && install == "1")
+                                {
+                                    sb.AppendLine("  " + subKeyName + "  " + name + "  SP" + sp);
+                                }
+                                else if (install == "1")
+                                {
+                                    sb.AppendLine("  " + subKeyName + "  " + name);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return sb.ToString();
+        }
+    
+    
+    
+    
+    }  // class Amblib
+}  // namespace Ambiesoft
