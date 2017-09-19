8f515260a33bc7362407626cd4c2b4f12d7aa033
zelda <zelda@THEXP>
2017-09-19 11:56:28 +0900

    non font

--- AmbLib/AmbLib.cs
+++ AmbLib/AmbLib.cs
@@ -574,15 +574,25 @@ namespace Ambiesoft
         public static void SetFontAll(Control cnt)
         {
             if (cnt is Form)
-            { } // cnt.Font = SystemFonts.CaptionFont;
+            {
+                // cnt.Font = SystemFonts.CaptionFont;
+            }
             else if (cnt is Button)
-                cnt.Font = SystemFonts.MenuFont;
+            {
+                // cnt.Font = SystemFonts.DialogFont;// MenuFont;
+            }
             else if (cnt is TextBox)
-                cnt.Font = SystemFonts.MenuFont;
+            {
+                // cnt.Font = SystemFonts.IconTitleFont;
+            }
             else if (cnt is Label)
-                cnt.Font = SystemFonts.MenuFont;
+            {
+                // cnt.Font = SystemFonts.MenuFont;
+            }
             else if (cnt is CheckBox)
-                cnt.Font = SystemFonts.MenuFont;
+            {
+                // cnt.Font = SystemFonts.MenuFont;
+            }
 
             foreach (Control c in cnt.Controls)
             {
