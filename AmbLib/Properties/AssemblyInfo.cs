d751d8d1b33ffa279532224dc1ef7e13d00a9101
ambiesoft <ambiesoft.trueff@gmail.com>
2022-10-07 00:58:09 +0900

    Revert .NET version to 4.0

--- AmbLib/Properties/AssemblyInfo.cs
+++ AmbLib/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("e1de976a-ddc3-4f41-8c7a-8b9382480744")]
 
-[assembly: AssemblyVersion("2.0.1.*")]
+[assembly: AssemblyVersion("1.0.41.*")]
 
