f0355d10c2e0a9331aa4b14a7950b99477b058bf
ambiesoft <ambiesoft.trueff@gmail.com>
2023-07-06 02:06:48 +0900

    Added GetOrdinalString

--- AmbLib/AmbLib.cs
+++ AmbLib/AmbLib.cs
@@ -2227,5 +2227,31 @@ namespace Ambiesoft
             }
             return sPost;
         }
+
+        // https://stackoverflow.com/a/20175
+        public static string GetOrdinalString(int num)
+        {
+            if (num <= 0) return num.ToString();
+
+            switch (num % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return num + "th";
+            }
+
+            switch (num % 10)
+            {
+                case 1:
+                    return num + "st";
+                case 2:
+                    return num + "nd";
+                case 3:
+                    return num + "rd";
+                default:
+                    return num + "th";
+            }
+        }
     }  // class Amblib
 }  // namespace Ambiesoft
